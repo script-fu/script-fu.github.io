ad294db303604640f6959d8f29d99f06b471f840
Mark Sweeney <121123014+script-fu@users.noreply.github.com>
2022-12-20 22:38:38 +0000

    Add files via upload

---
+++ curveDropMid.scm
@@ -0,0 +1,18 @@
+(define (curveDropMid img drawable)
+	(let*
+	()
+	(curve3Value img drawable 0 0 160 65 255 255)
+	(gimp-displays-flush) 
+	)
+)
+
+(script-fu-register "curveDropMid"
+	"<Image>/Script-Fu/curveDropMid"
+	"apply a specific curve to the image intensity levels"
+	"Mark Sweeney"
+	"copyright 2022, Mark Sweeney"
+	"2022"
+	"*"
+	SF-IMAGE       "Image"           		0
+	SF-DRAWABLE    "Drawable"        		0
+)
