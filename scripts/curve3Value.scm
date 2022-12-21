319a2c6cea450d80eb310f0cee1c882780b59036
Mark Sweeney <121123014+script-fu@users.noreply.github.com>
2022-12-21 16:56:23 +0000

    Update curve3Value.scm

--- scripts/curve3Value.scm
+++ scripts/curve3Value.scm
@@ -1,3 +1,11 @@
+;These scripts are free software: you can redistribute it and/or modify it under 
+;the terms of the GNU General Public License as published by the Free Software 
+;Foundation;
+
+;This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+;WARRANTY without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+;PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
 (define (curve3Value img drawable x1 y1 x2 y2 x3 y3)
 	(let*
 		(
